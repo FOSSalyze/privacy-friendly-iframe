3dc98ae7b0a80bc2104e8f70934ee0cd903ac904
Timothy Gillespie <timothy@gillespie.eu>
2024-02-24 01:00:58 +0100

    Make pf-iframe configurable via window variables

--- src/pf-iframe.ts
+++ src/pf-iframe.ts
@@ -1,5 +1,22 @@
 // Maps attributes from the PrivacyFriendlyIFrame to the iFrame element
 // Usually it remains 1:1
+interface PFIFrameConfig {
+    onConsent?: () => any;
+    generateConsentTemplate?: (width: string, height: string, src: string, self: HTMLElement) => string;
+    fetchConsentElement?: (self: HTMLElement) => HTMLElement | null;
+    checkExistingConsent?: (self: HTMLElement) => boolean;
+}
+
+
+interface Window {
+    _pfiFrameConfig?: {
+        default?: PFIFrameConfig;
+        byId?: {
+            [id: string]: PFIFrameConfig;
+        };
+    }
+}
+
 const attributeMap = new Map<string, string>([
     ['width', 'width'],
     ['height', 'height'],
@@ -16,6 +33,27 @@ const attributeMap = new Map<string, string>([
 
 const observedAttributes = Array.from(Object.keys(attributeMap));
 
+const fallbackDefaultConfig: Required<PFIFrameConfig> = {
+    onConsent: () => {},
+    generateConsentTemplate: (width: string, height: string, src: string, self: HTMLElement) => `
+    <div class="pf-iframe--consent-container" style="width: ${width}px; height: ${height}px">
+        <div class="pf-iframe--consent-content-container">
+            <p class="pf-iframe--consent-message">Do you want to view this content? It will transmit data to ${src}.</p>
+            <button class="pf-iframe--consent-button">Yes</button>
+        </div>
+    </div>`,
+    fetchConsentElement: (self) => self.querySelector('button'),
+    checkExistingConsent: (self) => false
+
+}
+
+const getConfig: (id?: string | null) => Required<PFIFrameConfig> = (id?: string | null) => {
+    if (id != null && window._pfiFrameConfig?.byId != null) {
+        return {...fallbackDefaultConfig, ...window._pfiFrameConfig?.default, ...(window._pfiFrameConfig?.byId[id] ?? {})};
+    }
+    return {...fallbackDefaultConfig, ...(window._pfiFrameConfig?.default ?? {})};
+};
+
 class PrivacyFriendlyIFrame extends HTMLElement {
     iFrameElement = document.createElement('iframe') as HTMLIFrameElement;
     private _mounted = false;
@@ -23,20 +61,29 @@ class PrivacyFriendlyIFrame extends HTMLElement {
         super();
     }
 
-    generateConsentTemplate = (width: string | null, height: string | null) => `
-    <div class="pf-iframe--consent-container" style="width: ${width ?? '300'}px; height: ${height ?? '150'}px">
-        <div class="pf-iframe--consent-content-container">
-            <p class="pf-iframe--consent-message">Do you want to view this content? It will transmit data to ${this.getAttribute('src')}.</p>
-            <button class="pf-iframe--consent-button">Yes</button>
-        </div>
-    </div>`;
+    config = getConfig(this.getAttribute('id'));
 
-    onConsent = (listener: () => any) => this.querySelector('button')?.addEventListener('click', listener);
-    
+    // should default to 300x150 pixels when no size is specified per convention
+    generateConsentTemplate = this.config.generateConsentTemplate
+
+    fetchConsentElement = this.config.fetchConsentElement;
+    checkExistingConsent = this.config.checkExistingConsent;
+    onConsent = this.config?.onConsent;
+
     connectedCallback() {
-        // should default to 300x150 pixels when no size is specified per convention
-        this.innerHTML = this.generateConsentTemplate(this.getAttribute('width'), this.getAttribute('height')); 
-        this.onConsent(this.accept.bind(this));
+        if(this.checkExistingConsent(this)) {
+            this.mountIFrame();
+        } else {
+            const width = this.getAttribute('width') ?? '300';
+            const height = this.getAttribute('height') ?? '150';
+            const src = this.getAttribute('src') ?? 'ERROR';
+            
+            this.innerHTML = this.generateConsentTemplate(width, height, src, this); 
+            this.fetchConsentElement(this)?.addEventListener('click', () => {
+                this.onConsent(); 
+                this.accept()
+            });
+        }
     }
 
     accept() {
