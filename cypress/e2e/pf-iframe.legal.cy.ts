967d4157418add49f9daf5ec474562fefebf2384
Timothy Gillespie <timothy@gillespie.eu>
2024-02-27 04:51:44 +0100

    Remove oddly copied extra describe

--- cypress/e2e/pf-iframe.legal.cy.ts
+++ cypress/e2e/pf-iframe.legal.cy.ts
@@ -1,24 +1,22 @@
-import { runConfigurations } from "./run-configurations";
+import { runConfigurations } from './run-configurations';
 
 for (const config of runConfigurations) {
-    describe(`technical specification for ${config.name} products`, () => {
-        describe('legal specification', () => {
-            it('no data transfer by default', () => {
-                cy.intercept('https://www.youtube.com/*').as('anyRequest');
+    describe(`legal specification for ${config.name} products`, () => {
+        it('no data transfer by default', () => {
+            cy.intercept('https://www.youtube.com/*').as('anyRequest');
 
-                cy.visit(config.baseUrl);
+            cy.visit(config.baseUrl);
 
-                // Not ideal, but the best I could think of - instantly quickly @anyRequest might hide a potential issue 
-                cy.wait(2000);
+            // Not ideal, but the best I could think of - instantly quickly @anyRequest might hide a potential issue
+            cy.wait(2000);
 
-                // No requests made before button click
-                cy.get('@anyRequest.all').should('be.empty');
+            // No requests made before button click
+            cy.get('@anyRequest.all').should('be.empty');
 
-                cy.get('button').click();
+            cy.get('button').click();
 
-                // Requests made after button click
-                cy.get('@anyRequest.all').should('not.be.empty');
-            });
+            // Requests made after button click
+            cy.get('@anyRequest.all').should('not.be.empty');
         });
     });
 }
