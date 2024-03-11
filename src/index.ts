fb93e345a4716e0376b645ab179efd2688b7281e
Timothy Gillespie <timothy@gillespie.eu>
2024-03-11 01:55:51 +0100

    Add and utilize index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './configuration';
+export * from './assign-translation-to-window';
+export * from './pf-iframe.class';
+export * from './types';
+export * from './translations/en';
+export * from './translations/de';
